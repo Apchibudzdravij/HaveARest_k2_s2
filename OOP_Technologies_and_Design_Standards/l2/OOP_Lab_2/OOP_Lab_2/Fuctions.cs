4b9254ff120dbe2978b6079d7be05f5da702a76b
Apchibudzdravij <90217287+Apchibudzdravij@users.noreply.github.com>
2022-04-02 19:51:36 +0300

    2nd lab uploaded
    To make drawing result better, you need to change in Shop.cs method Show() to the version in 3rd lab (or 4th, or 5th) :)

---
+++ OOP_Technologies_and_Design_Standards/l2/OOP_Lab_2/OOP_Lab_2/Fuctions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace OOP_Lab_2
+{
+    interface IXMLSaveable
+    {
+        void Applying();
+        void Saving();
+        void Loading();
+    }
+
+    public partial class Form1: IXMLSaveable
+    {
+        public void Applying()
+        {
+            pictureBox1.Visible = false;
+            if (!Regex.IsMatch(emailbox.Text, @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", RegexOptions.IgnoreCase))
+            {
+                validation.Text = "E-mail is wrong!";
+                return;
+            }
+            Storekeeper storekeeper = new Storekeeper("Ivan", 4, "ul.Pervaja, 14-2");
+            Product currentProduct = new Product(storekeeper);
+            switch (listBox1.SelectedItem.ToString())
+            {
+                case "s": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 1.0); break;
+                case "m": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 2.0); break;
+                case "l": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 3.5); break;
+                case "xl": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 6.75); break;
+                default: currentProduct.AddSize("S", 1.0); break;
+            }
+            currentProduct.AddName(textBox1.Text);
+            currentProduct.AddEmail(emailbox.Text);
+            currentProduct.AddDate(dateTimePicker1.Value);
+            string[] words = modelBox1.Text.Split(' ');
+            currentProduct.AddModel(words[0], words[1], 24, 46.99f);
+            validation.Text = currentProduct.Show();
+        }
+
+        public void Saving()
+        {
+            Storekeeper storekeeper = new Storekeeper("Ivan", 4, "ul.Pervaja, 14-2");
+            Product currentProduct = new Product(storekeeper);
+            switch (listBox1.SelectedItem.ToString())
+            {
+                case "s": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 1.0); break;
+                case "m": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 2.0); break;
+                case "l": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 3.5); break;
+                case "xl": currentProduct.AddSize(listBox1.SelectedItem.ToString(), 6.75); break;
+                default: currentProduct.AddSize("s", 1.0); break;
+            }
+            currentProduct.AddName(textBox1.Text);
+            currentProduct.AddEmail(emailbox.Text);
+            currentProduct.AddDate(dateTimePicker1.Value);
+            string[] words = modelBox1.Text.Split(' ');
+            currentProduct.AddModel(words[0], words[1], 24, 46.99f);
+
+            File.Delete("D:\\ExtendedData\\Laboratory\\ООТПиСП\\l2\\OOP_Lab_2\\memory.xml");
+            XmlSerializer XMLmatter = new XmlSerializer(typeof(Product));
+            using (FileStream fx = new FileStream("D:\\ExtendedData\\Laboratory\\ООТПиСП\\l2\\OOP_Lab_2\\memory.xml", FileMode.OpenOrCreate))
+            {
+                XMLmatter.Serialize(fx, currentProduct);
+            }
+        }
+
+        public void Loading()
+        {
+            XmlSerializer XMLmatter = new XmlSerializer(typeof(Product));
+            using (FileStream fx = new FileStream("D:\\ExtendedData\\Laboratory\\ООТПиСП\\l2\\OOP_Lab_2\\memory.xml", FileMode.OpenOrCreate))
+            {
+                Product currentProduct = XMLmatter.Deserialize(fx) as Product;
+                textBox1.Text = currentProduct.customer;
+                emailbox.Text = currentProduct.email;
+                dateTimePicker1.Value = currentProduct.date;
+                modelBox1.Text = currentProduct.name + " " + currentProduct.id;
+                listBox1.SelectedIndex = listBox1.FindString(currentProduct.size);
+            }
+        }
+    }
+}
---
+++ OOP_Technologies_and_Design_Standards/l2/OOP_Lab_2/OOP_Lab_2/Shop.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OOP_Lab_2
+{
+    interface IProduct
+    {
+        void AddEmail(string value);
+        void AddName(string value);
+        void AddSize(string sz, double weight);
+        void AddModel(string nm, string d, int cnt, float prc);
+        void AddDate(DateTime DtTm);
+        string Show();
+    }
+
+    [Serializable]
+    public class Product : IProduct
+    {
+        public string name;    //model
+        public string id;      //model
+        public string size;     //size
+        public DateTime date;  //date
+        public  string customer;//name
+        public string email;   //email
+        [NonSerialized]
+        double weight;  //size
+        string type = "T-Shirt";
+        int count;      //model
+        float price;    //model
+        Storekeeper mother;
+        public Product(Storekeeper woman)
+        {
+            this.mother = woman;
+            this.customer = "";
+            this.email = "";
+            this.name = "";
+            this.id = "";
+            this.size = "S";
+            this.weight = 0.0;
+            this.date = DateTime.Now;
+            this.count = 0;
+            this.price = 0;
+        }
+        public Product()
+        {
+            this.mother = null;
+            this.customer = "";
+            this.email = "";
+            this.name = "";
+            this.id = "";
+            this.size = "S";
+            this.weight = 0.0;
+            this.date = DateTime.Now;
+            this.count = 0;
+            this.price = 0;
+        }
+        public void AddEmail(string value) { this.email = value; }
+        public void AddName(string value) { this.customer = value; }
+        public void AddSize(string sz, double wght)
+        {
+            this.size = sz; this.weight = wght;
+        }
+        public void AddModel(string nm, string d, int cnt, float prc)
+        {
+            this.name = nm; this.id = d; this.count = cnt; this.price = prc;
+        }
+        public void AddDate(DateTime DtTm) { this.date = DtTm; }
+        public string Show()
+        {
+            string result = "";
+            result += this.customer + "\n";
+            result += this.email + "\n";
+            result += this.type + "\n";
+            result += this.name + "\n";
+            result += this.id + "\n";
+            result += this.size.ToUpper() + "\n";
+            result += this.weight + "\n";
+            result += this.date.ToLongDateString() + "\n";
+            result += this.count + "\n";
+            result += this.price + "\n";
+            return result;
+        }
+    }
+
+    public class Storekeeper
+    {
+        string name;
+        int experience;
+        string address;
+        public Storekeeper(string nm, int xprnc, string ddrs)
+        {
+            this.name = nm; this.experience = xprnc; this.address = ddrs;
+        }
+    }
+}
