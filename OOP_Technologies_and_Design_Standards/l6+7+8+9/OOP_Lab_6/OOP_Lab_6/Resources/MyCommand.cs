a84d33a959cfe3563257ca56c7240e3fce60563a
Apchibudzdravij <90217287+Apchibudzdravij@users.noreply.github.com>
2022-05-27 23:08:21 +0300

    You can also use it as 13th lab
    (I used MVVM pattern in 6+7+8+9)

---
+++ OOP_Technologies_and_Design_Standards/l6+7+8+9/OOP_Lab_6/OOP_Lab_6/Resources/MyCommand.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OOP_Lab_6.Resources
+{
+    public class MyCommand
+    {
+        private static RoutedUICommand query;
+        static MyCommand()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.CapsLock, ModifierKeys.Control, "Ctrl+CapsLock"));
+            query = new RoutedUICommand("Query", "Query", typeof(MyCommand), inputs);
+        }
+        public static RoutedUICommand Query
+        {
+            get { return query; }
+        }
+    }
+}
